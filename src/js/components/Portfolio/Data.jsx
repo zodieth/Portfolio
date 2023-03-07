04a80e2c7f556cb78e1d1b111854d3514eb59e19
zodieth <contact.prodzodi@gmail.com>
2023-03-07 09:39:51 -0300

    proyects

--- src/js/components/Portfolio/Data.jsx
+++ src/js/components/Portfolio/Data.jsx
@@ -1,12 +1,28 @@
 import Work1 from "../../../media/pokemons.png";
+import Work2 from "../../../media/2.png";
+import Work3 from "../../../media/netflix.png";
 
 export const projectsData = [
+  // {
+  //   id: 1,
+  //   image: Work1,
+  //   Title: "Pokedex",
+  //   Category: "Personal projects",
+  //   Demo: "https://pi-pokemon-main-sigma.vercel.app/home",
+  // },
   {
-    id: 1,
-    image: Work1,
-    Title: "Pokedex",
+    id: 2,
+    image: Work2,
+    Title: "AllTech Ecommerce",
     Category: "Personal projects",
-    Demo: "https://pi-pokemon-main-sigma.vercel.app/home",
+    Demo: "https://client-ochre-five.vercel.app/",
+  },
+  {
+    id: 3,
+    image: Work3,
+    Title: "Netflix Clone",
+    Category: "Personal projects",
+    Demo: "https://netflix-clon-s4n3.vercel.app/",
   },
 ];
 
